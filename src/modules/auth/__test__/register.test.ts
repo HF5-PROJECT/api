4069326175f3c2c5cfbed4f68ead66e16631f6dd
frederikpyt <frederikpyt@gmail.com>
2023-02-10 09:28:21 +0100

    #15 - Changed the register endpoint validation rules

--- src/modules/auth/__test__/register.test.ts
+++ src/modules/auth/__test__/register.test.ts
@@ -17,7 +17,7 @@ describe("POST /api/auth/register", () => {
         await fastify.close();
     });
 
-    it("should return status 200 and create a user", async () => {
+    it("should return status 201 and create a user", async () => {
         const response = await fastify.inject({
             method: "POST",
             url: "/api/auth/register",
@@ -26,7 +26,7 @@ describe("POST /api/auth/register", () => {
                 email: "joe@biden.com",
                 address:
                     "1600 Pennsylvania Avenue NW, Washington, DC 20500, USA",
-                password: "1234",
+                password: "12345678",
             },
         });
 
@@ -38,13 +38,32 @@ describe("POST /api/auth/register", () => {
         });
     });
 
+    it("should return status 201 and create a user, when no address has been provided", async () => {
+        const response = await fastify.inject({
+            method: "POST",
+            url: "/api/auth/register",
+            payload: {
+                name: "Joe Biden",
+                email: "joe@biden.com",
+                password: "12345678",
+            },
+        });
+
+        expect(response.statusCode).toBe(201);
+        expect(response.json()).toEqual({
+            name: "Joe Biden",
+            email: "joe@biden.com",
+            address: null,
+        });
+    });
+
     it("should return status 400, when email is already in use", async () => {
         await prisma.user.create({
             data: {
                 name: "Joe Biden the 1st",
                 email: "joe@biden.com",
                 address: "",
-                password: "1234",
+                password: "12345678",
             },
         });
 
@@ -56,7 +75,7 @@ describe("POST /api/auth/register", () => {
                 email: "joe@biden.com",
                 address:
                     "1600 Pennsylvania Avenue NW, Washington, DC 20500, USA",
-                password: "1234",
+                password: "12345678",
             },
         });
 
@@ -77,7 +96,7 @@ describe("POST /api/auth/register", () => {
                 email: "joebiden.com",
                 address:
                     "1600 Pennsylvania Avenue NW, Washington, DC 20500, USA",
-                password: "1234",
+                password: "12345678",
             },
         });
 
@@ -97,7 +116,7 @@ describe("POST /api/auth/register", () => {
                 name: "Joe Biden",
                 address:
                     "1600 Pennsylvania Avenue NW, Washington, DC 20500, USA",
-                password: "1234",
+                password: "12345678",
             },
         });
 
@@ -105,6 +124,27 @@ describe("POST /api/auth/register", () => {
         expect(response.json()).toEqual({
             error: "Bad Request",
             message: "body must have required property 'email'",
+            statusCode: 400,
+        });
+    });
+
+    it("should return status 400, when email is empty", async () => {
+        const response = await fastify.inject({
+            method: "POST",
+            url: "/api/auth/register",
+            payload: {
+                name: "Joe Biden",
+                email: "",
+                address:
+                    "1600 Pennsylvania Avenue NW, Washington, DC 20500, USA",
+                password: "12345678",
+            },
+        });
+
+        expect(response.statusCode).toBe(400);
+        expect(response.json()).toEqual({
+            error: "Bad Request",
+            message: "body/email must NOT have fewer than 1 characters",
             statusCode: 400,
         });
     });
@@ -129,6 +169,48 @@ describe("POST /api/auth/register", () => {
         });
     });
 
+    it("should return status 400, when password is empty", async () => {
+        const response = await fastify.inject({
+            method: "POST",
+            url: "/api/auth/register",
+            payload: {
+                name: "Joe Biden",
+                email: "joe@biden.com",
+                address:
+                    "1600 Pennsylvania Avenue NW, Washington, DC 20500, USA",
+                password: "",
+            },
+        });
+
+        expect(response.statusCode).toBe(400);
+        expect(response.json()).toEqual({
+            error: "Bad Request",
+            message: "body/password must NOT have fewer than 8 characters",
+            statusCode: 400,
+        });
+    });
+
+    it("should return status 400, when password is less than 8 characters", async () => {
+        const response = await fastify.inject({
+            method: "POST",
+            url: "/api/auth/register",
+            payload: {
+                name: "Joe Biden",
+                email: "joe@biden.com",
+                address:
+                    "1600 Pennsylvania Avenue NW, Washington, DC 20500, USA",
+                password: "1234567",
+            },
+        });
+
+        expect(response.statusCode).toBe(400);
+        expect(response.json()).toEqual({
+            error: "Bad Request",
+            message: "body/password must NOT have fewer than 8 characters",
+            statusCode: 400,
+        });
+    });
+
     it("should return status 400, when no name has been provided", async () => {
         const response = await fastify.inject({
             method: "POST",
@@ -137,7 +219,7 @@ describe("POST /api/auth/register", () => {
                 email: "joe@biden.com",
                 address:
                     "1600 Pennsylvania Avenue NW, Washington, DC 20500, USA",
-                password: "1234",
+                password: "12345678",
             },
         });
 
@@ -149,21 +231,23 @@ describe("POST /api/auth/register", () => {
         });
     });
 
-    it("should return status 400, when no address has been provided", async () => {
+    it("should return status 400, when name is empty", async () => {
         const response = await fastify.inject({
             method: "POST",
             url: "/api/auth/register",
             payload: {
-                name: "Joe Biden",
+                name: "",
                 email: "joe@biden.com",
-                password: "1234",
+                address:
+                    "1600 Pennsylvania Avenue NW, Washington, DC 20500, USA",
+                password: "12345678",
             },
         });
 
         expect(response.statusCode).toBe(400);
         expect(response.json()).toEqual({
             error: "Bad Request",
-            message: "body must have required property 'address'",
+            message: "body/name must NOT have fewer than 1 characters",
             statusCode: 400,
         });
     });
