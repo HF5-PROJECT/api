73c41d2c5aee16849bead83ea8ae4215c66ac852
Kristian Binau <kristian@binau.me>
2023-02-16 14:05:27 +0100

    #18 - Run migrations

--- prisma/migrations/20230216130300_hotel/migration.sql
+++ prisma/migrations/20230216130300_hotel/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE `Hotel` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(50) NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
     `description` TEXT NULL,
     `address` VARCHAR(255) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
